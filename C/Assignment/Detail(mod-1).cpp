9d896a4580c8c8dce3788438f2b0498cf7649788
khushi13124 <khushi13124@gmail.com>
2023-03-03 18:20:29 +0530

    complete
    complete

---
+++ C/Assignment/Detail(mod-1).cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	printf("My name is Khushi Patel\n");       //a
+	printf("My birth date is 13/12/2004");     //b
+	printf("My age is 18");                    //c
+	printf("Address:4,Darshan Bungalows,Vibhusha Bungalows Road,Bopal\n");   //d
+	printf("Mob no.: 7624016542");
+}
